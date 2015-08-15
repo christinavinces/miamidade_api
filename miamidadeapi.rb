da491687e69f5dde0bf1caa211394207a53c2daa
cvinces <christina.vinces@gmail.com>
2015-08-15 01:03:16 -0400

    add permit average calculation

--- miamidadeapi.rb
+++ miamidadeapi.rb
@@ -1,17 +1,17 @@
 require 'httparty'
 
 def costs_by_category(category_number)
-	data = HTTParty.get('https://opendata.miamidade.gov/resource/awsz-tanw.json?$limit=40000')
-	matches = []
-	data.each do |permit|
-		matches << permit["estimatedvalue"].to_i if permit["categories"] == category_number.to_s && permit["estimatedvalue"].to_i != 0
-	end
-	return matches.sort
+  data = HTTParty.get('https://opendata.miamidade.gov/resource/awsz-tanw.json?$limit=40000')
+  matches = []
+  data.each do |permit|
+  	matches << permit["estimatedvalue"].to_i if permit["categories"] == category_number.to_s && permit["estimatedvalue"].to_i != 0
+  end
+  return matches.sort
 end
 
 def average_cost_by_category(category_number)
-	all_costs = costs_by_category(category_number)
-	all_costs.map(&:to_i).inject(:+) / all_costs.count
+  all_costs = costs_by_category(category_number)
+  all_costs.map(&:to_i).inject(:+) / all_costs.count
 end
 
 category_number = 55
