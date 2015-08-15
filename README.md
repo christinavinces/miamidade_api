42a1b0f19c951279d1e9f4ffbaf14087634f1cfe
cvinces <christina.vinces@gmail.com>
2015-08-15 01:01:18 -0400

    add permit average calculation

---
+++ README.md
@@ -0,0 +1,3 @@
+This program takes averages on building permit costs per permit category from the Miami-Dade County Open Data Portal
+
+https://opendata.miamidade.gov/
---
+++ miamidadeapi.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+def costs_by_category(category_number)
+	data = HTTParty.get('https://opendata.miamidade.gov/resource/awsz-tanw.json?$limit=40000')
+	matches = []
+	data.each do |permit|
+		matches << permit["estimatedvalue"].to_i if permit["categories"] == category_number.to_s && permit["estimatedvalue"].to_i != 0
+	end
+	return matches.sort
+end
+
+def average_cost_by_category(category_number)
+	all_costs = costs_by_category(category_number)
+	all_costs.map(&:to_i).inject(:+) / all_costs.count
+end
+
+category_number = 55
+puts average_cost_by_category(category_number)
