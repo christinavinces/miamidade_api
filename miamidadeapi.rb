c40f50547346aab391a5016f48649486dbf1ba24
christinavinces <christina.vinces@gmail.com>
2015-08-15 01:58:04 -0400

    Update miamidadeapi.rb

--- miamidadeapi.rb
+++ miamidadeapi.rb
@@ -4,7 +4,7 @@ def costs_by_category(category_number)
   data = HTTParty.get('https://opendata.miamidade.gov/resource/awsz-tanw.json?$limit=40000')
   matches = []
   data.each do |permit|
-  	matches << permit["estimatedvalue"].to_i if permit["categories"] == category_number.to_s && permit["estimatedvalue"].to_i != 0
+    matches << permit["estimatedvalue"].to_i if permit["categories"] == category_number.to_s && permit["estimatedvalue"].to_i != 0
   end
   return matches.sort
 end
